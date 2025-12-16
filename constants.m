5692594f0ec3d91bea99259f35203a6f951d740f
Emisca9 <emicacciu@gmail.com>
2025-12-16 14:19:36 +0100

    some changes to try to fix error in time = 0

--- constants.m
+++ constants.m
@@ -1,3 +1,15 @@
+%% Regulation controller
+
 k_v = 0.1;
 k_w = 0.1;
 k_delta = 0.1;
+
+%% Linear Trajectory Tracking Controller constants:
+
+a = 3;
+xi = 0.6;
+
+%% Non linear Trajectory Tracking Controller constants:
+
+b = 5;
+xi = 0.6;
