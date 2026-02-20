a02955311e8103a1e6b15725ff01666faf903382
davidepillon001 <davide.pillon04@gmail.com>
2026-02-20 11:50:59 +0100

    update NL_trajectory

--- RC2_Project_git/RC2_Project-main/Main/simulation.m
+++ RC2_Project_git/RC2_Project-main/Main/simulation.m
@@ -3,8 +3,8 @@ a = 3;  % >0
 l_xi = 0.7;  % (0,1)
 
 %% Non linear Trajectory Tracking Controller constants
-b = 2;  % >0
-nl_xi = 0.7;  % (0,1)
+b = 5;  % >0
+nl_xi = 0.4;  % (0,1)
 
 %% Parking box pose
 x_box     = 850;
@@ -21,8 +21,8 @@ k2 = 1.5;
 k3 = 2;
 
 %% Simulation Variables
-%shift_time = 3;  % trajectory tracking time (before regulation)
-stop_time = 15;
+shift_time = 20;  % trajectory tracking time (before regulation)
+stop_time = shift_time + 10;
 
 %% desired trajectory generation
 S = load('trajectory.mat');   % contains S.traj.xy and S.traj.t
@@ -36,12 +36,12 @@ xy = S.traj.xy;               % [N x 2]
 t  = S.traj.t;                % [N x 1] [belongs to (0,1)]
 
 % last (x,y) to be tracked
-last_t = size(xy, 1)-1;
+%last_t = size(xy, 1)-1;
 %assignin('base','last_t', last_t);
 
-%Tfinal = shift_time;           % seconds
-%t = t * Tfinal;
+Tfinal = shift_time;           % seconds
+t = t * Tfinal;
 
-ref = timeseries(xy);      % ref.Data is Nx2: [x_d y_d]
+ref = timeseries(xy, t);      % ref.Data is Nx2: [x_d y_d]
 
 assignin('base','ref', ref);
